7eeaaab94858ceb8a95b431e82ebaa203ebbac59
lorincmeszaros <64486996+lorincmeszaros@users.noreply.github.com>
2022-11-16 10:04:19 +0100

    Update model_nabaripoma.py

--- model/model_nabaripoma.py
+++ model/model_nabaripoma.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 15 15:44:47 2022
-
-@author: lorinc
+Model to assess the combined bio-physical and behavioural developments
+along an imaginary river stretch in the coastal zone of Bangladesh.
+This model has been implemented in Python.
+This demontrsation has been developed as part of the 2022 research project: 
+'Integrated Assessment Modelling of Tidal River Management in Bangladesh'.
 """
 import numpy as np
 import math as math
@@ -25,7 +27,6 @@ slr2100 = model_params['slr2100']
 subsidence = model_params['subsidence']
 sedrate = model_params['sedrate']
 trmsedrate = model_params['trmsedrate']
-showvar = model_params['showvar']
 mslstart = 0.00
 startyear = 2022
 endyear = 2100
@@ -37,43 +38,95 @@ msl = 0.00
    
 # Read the elevation from elevation.asc
 elevmat = np.loadtxt(r'p:\11208012-011-nabaripoma\Model\Mesa\NaBaRiPoMa01\elevation.asc', delimiter = ' ', skiprows = 6) * 0.01
+#plot
+plt.matshow(elevmat)
+plt.title('elevation')
+plt.colorbar()
+plt.show()
 
 # read the location of rivers from file rs.asc
 rsmat = np.loadtxt(r'p:\11208012-011-nabaripoma\Model\Mesa\NaBaRiPoMa01\rs.asc', delimiter = ' ', skiprows = 6)
+#plot
+plt.matshow(rsmat)
+plt.title('location of rivers')
+plt.colorbar()
+plt.show()
 
 # read the location of polders from file polder.asc
 polmat = np.loadtxt(r'p:\11208012-011-nabaripoma\Model\Mesa\NaBaRiPoMa01\Polders.asc', delimiter = ' ', skiprows = 6)
+#plot
+plt.matshow(polmat)
+plt.title('location of polders')
+plt.colorbar()
+plt.show()
 
 #initial pcraster calculation
 #set clonemap
 pcr.setclone(height, width, 1, 0, 0)
 #set floodelevmat with polders 10m higher
 floodelevmat = np.where(polmat > 0, elevmat + 10., elevmat)
+#plot
+plt.matshow(floodelevmat)
+plt.title('flood elevation')
+plt.colorbar()
+plt.show()
+
 #convert numpay arrays to PCR rasters
 pcrelev = pcr.numpy2pcr(pcr.Scalar, elevmat, -999.)
-pcr.report(pcrelev, 'elevation.map')
+pcr.report(pcrelev, r'p:\11208012-011-nabaripoma\Model\Python\results\elevation.map')
 pcrfloodelev = pcr.numpy2pcr(pcr.Scalar, floodelevmat, -999.)
-pcr.report(pcrfloodelev, 'elevation.map')
+pcr.report(pcrfloodelev, r'p:\11208012-011-nabaripoma\Model\Python\results\floodelevation.map')
 #create ldd
 pcr.setglobaloption('lddin')
 pcr.setglobaloption('unittrue')
 pcrldd = pcr.lddcreate(pcrfloodelev,9999999,9999999,9999999,9999999)
-pcr.report(pcrldd,'ldd.map')
+pcr.report(pcrldd, r'p:\11208012-011-nabaripoma\Model\Python\results\ldd.map')
+lddmat = pcr.pcr2numpy(pcrldd,-999)
+#plot
+plt.matshow(lddmat)
+plt.title('ldd')
+plt.colorbar()
+plt.show()
+
 #create river and sea array
 rivmat = np.where(rsmat == 2, 1, 0)
+#plot
+plt.matshow(rivmat)
+plt.title('river')
+plt.colorbar()
+plt.show()
+
 seamat = np.where(rsmat == 1, 1, 0)
+#plot
+plt.matshow(seamat)
+plt.title('sea')
+plt.colorbar()
+plt.show()
+
 #convert river and sea array to map
 pcrriv = pcr.numpy2pcr(pcr.Boolean, rivmat, -999.)
 pcrsea = pcr.numpy2pcr(pcr.Boolean, seamat, -999.)
 #calculate distance to river over ldd
 pcrdist2riv = pcr.ldddist(pcrldd,pcrriv,1.)
 dist2rivmat = pcr.pcr2numpy(pcrdist2riv,-999)
+#plot
+plt.matshow(dist2rivmat)
+plt.title('dist2riv_ldd')
+plt.colorbar()
+plt.show()
+
 pcr.report(pcrdist2riv,r'p:\11208012-011-nabaripoma\Model\Mesa\NaBaRiPoMa01\dist2riv.map')
+
 #calculate distance to sea over ldd
 pcrdist2sea = pcr.ldddist(pcrldd,pcrsea,1.)
 dist2seamat = pcr.pcr2numpy(pcrdist2sea,-999)
-pcr.report(pcrdist2sea,r'p:\11208012-011-nabaripoma\Model\Mesa\NaBaRiPoMa01\dist2sea.map')
+#plot
+plt.matshow(dist2seamat)
+plt.title('dist2sea_ldd')
+plt.colorbar()
+plt.show()
 
+pcr.report(pcrdist2sea,r'p:\11208012-011-nabaripoma\Model\Mesa\NaBaRiPoMa01\dist2sea.map')
 
 for year in np.arange(startyear, endyear+1,1):
     """
@@ -97,7 +150,6 @@ for year in np.arange(startyear, endyear+1,1):
     dist2seamat = pcr.pcr2numpy(pcrdist2sea,-999)
     
     #Physics calculation
-    productivity = 0.0
     is_waterlogged = np.full(np.shape(elevmat),False)
     is_trm = np.full(np.shape(elevmat),False)
 
